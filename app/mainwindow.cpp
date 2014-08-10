8d5559ee34a6814cecfc2b34d94d6a4b41f49453
David Edmundson <kde@davidedmundson.co.uk>
2014-08-10 19:55:19 +0200

    port KUrl.equals

--- app/mainwindow.cpp
+++ app/mainwindow.cpp
@@ -1030,7 +1030,7 @@ void MainWindow::openDirUrl(const QUrl &url)
 {
     const QUrl currentUrl = d->mContextManager->currentDirUrl();
 
-    if (url.equals(currentUrl, QUrl::CompareWithoutTrailingSlash)) {
+    if (url == currentUrl) {
         return;
     }
 
@@ -1204,7 +1204,7 @@ void MainWindow::goToUrl(const QUrl &url)
     QUrl dirUrl = url;
     dirUrl = dirUrl.adjusted(QUrl::RemoveFilename);
     dirUrl.setPath(dirUrl.path() + "");
-    if (!dirUrl.equals(d->mContextManager->currentDirUrl(), QUrl::CompareWithoutTrailingSlash)) {
+    if (dirUrl != d->mContextManager->currentDirUrl()) {
         d->mContextManager->setCurrentDirUrl(dirUrl);
         d->mGvCore->addUrlToRecentFolders(dirUrl);
     }
